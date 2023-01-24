66fcfddd8340e2e9f74f3a0ae9e87a1e658cdd8c
vcchung <vcchung@gmail.com>
2023-01-24 18:31:04 +0800

    add mongo user

--- main.py
+++ main.py
@@ -1,18 +1,32 @@
 from flask import Flask, jsonify
 import os
 from flask_cors import CORS
+from pymongo import MongoClient
+from bson.json_util import dumps
 
 app = Flask(__name__)
 CORS(app)
 
+def get_db():
+    client=MongoClient("mongodb://mongo:jx5Y2trR0XXHiyUPTfdE@containers-us-west-170.railway.app:6336")
+    return client['flask-db']
+    
 @app.route('/')
 def index():
     return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
 
 @app.route('/user')
 def test():
-    return jsonify({"name": "testing user"})
+    return jsonify({"name": "testing user again"})
 
+@app.route('/mongo-user')
+def mongo_user():
+    print("calling mongo-user")
+    user=get_db()["user"].find_one()
+    print('result is {}'.format(user))
+    return dumps(user)
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
+
+
